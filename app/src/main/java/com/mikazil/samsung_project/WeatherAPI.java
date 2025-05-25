4e3d445d3131fe5909162ece7bb3490d9442a42b
Mika <akimivanov43@gmail.com>
2025-05-25 20:17:01 +0700

    Написано получение погоды по городу и координатам

--- app/src/main/java/com/mikazil/samsung_project/WeatherAPI.java
+++ app/src/main/java/com/mikazil/samsung_project/WeatherAPI.java
@@ -10,11 +10,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 public class WeatherAPI {
     private static final String API_KEY = "b37d8af2cceace36578fa5899ad6a9f8";
-    private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + API_KEY;
+    //private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + API_KEY;
     private static final String LOG_TAG = "WeatherAPI";
-    private String res;
 
-     public String getWeatherData() {
+
+    private String getWeather(String API_URL){
+        String res = null;
         HttpURLConnection connection = null;
         InputStream inputStream = null;
         try {
@@ -48,4 +49,17 @@ public class WeatherAPI {
         }
         return res;
     }
+
+    public String getWeatherDataByCity(String City) {
+        //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+         String API_URL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + "&appid=" + API_KEY;
+         return getWeather(API_URL);
+    }
+
+    public String getWeatherDataByCoordinates(String Latitude, String Longitude) {
+        //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+        String API_URL = "https://api.openweathermap.org/data/2.5/weather?lat="+ Latitude + "&lon=" + Longitude + "&appid=" + API_KEY;
+        return getWeather(API_URL);
+    }
+
 }
